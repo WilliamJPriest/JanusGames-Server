0eb3811606972c4f0c81fd5b0a25bbdc0d4249a8
williamjpriest <liuliu6wp@gmail.com>
2023-09-26 21:05:16 +0800

    upload server

---
+++ server.py
@@ -0,0 +1,41 @@
+from flask import Flask, request, jsonify
+import datetime
+import json
+import csv
+from flask_cors import CORS
+import mysql.connector
+
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="",
+  database="mbnewsletter"
+)
+
+mycursor = mydb.cursor()
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/newsletter', methods=['POST'])
+def submitNewsletter():
+    if request.method == 'POST':
+
+        name = request.form.get("name")
+        email = request.form.get("email")
+
+        response_data = {"message": "Newsletter submission successful", "name": name}
+        sql = "INSERT INTO clients (name, email) VALUES (%s, %s)"
+
+        val = (name, email)
+        mycursor.execute(sql, val)
+
+        mydb.commit()
+
+        print(mycursor.rowcount, "record inserted.")
+        return jsonify(response_data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
