c4a8ec42b76111f57445af4155d18efa71157423
williamjpriest <liuliu6wp@gmail.com>
2023-10-11 18:35:21 -0400

    test server

--- server.py
+++ server.py
@@ -1,41 +1,41 @@
-from flask import Flask, request, jsonify
-import datetime
 import json
-import csv
-from flask_cors import CORS
-import mysql.connector
-
+from typing import Self
 import mysql.connector
 
 mydb = mysql.connector.connect(
-  host="localhost",
-  user="root",
-  password="1234",
-  database="mbnewsletter"
+#   host="database",
+#   user="user",
+#   password="password",
+  database="newsletters"
 )
 
 mycursor = mydb.cursor()
 
-app = Flask(__name__)
-CORS(app)
 
-@app.route('/newsletter', methods=['POST'])
-def submitNewsletter():
-    if request.method == 'POST':
+def lambda_handler(event, context):
+    
+    name = event["name"]
+    email = event["email"]
+    
+    response_data = {"message": "Newsletter submission successful", "name": name}
+    sql = "INSERT INTO clients (name, email) VALUES (%s, %s)"
+    val = (name, email)
+    mycursor.execute(sql, val)
+    mydb.commit()
+    
+    print(mycursor.rowcount, "record inserted.")
+    
+    return {
+        'statusCode': 200,
+        'body': json.dumps(response_data)
+    }
 
-        name = request.form.get("name")
-        email = request.form.get("email")
+      
 
-        response_data = {"message": "Newsletter submission successful", "name": name}
-        sql = "INSERT INTO clients (name, email) VALUES (%s, %s)"
-
-        val = (name, email)
-        mycursor.execute(sql, val)
-
-        mydb.commit()
-
-        print(mycursor.rowcount, "record inserted.")
-        return jsonify(response_data)
+# def lambda_handler(event, context):
+#   return awsgi.response(app, event, context, base64_content_types={"image/png"})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    Self.run(debug=True)
+
+
