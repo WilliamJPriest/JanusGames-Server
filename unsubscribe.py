2a2db4a7fe764aede436dc2417bc44621e896d64
williamjpriest <liuliu6wp@gmail.com>
2023-10-16 09:46:07 -0400

    added unsubscribe example aws script

--- unsubscribe.py
+++ unsubscribe.py
@@ -15,19 +15,20 @@ def lambda_handler(event, context):
     print(event)
     
     email = event["email"]
-    print(email)
     response_data = {"message": "Unsubscribed", "email": email}
-    sql = "delete from clients where email = (email) VALUES (%s)"
-    val = (email)
+    
+    sql = "DELETE FROM clients WHERE email = %s"
+    val = (email,)  
     print(val)
+    
     mycursor.execute(sql, val)
     mydb.commit()
-    # return jsonify(response_data)
     
     return {
         'statusCode': 200,
         'body': json.dumps(response_data)
     }
+    
 
 if __name__ == '__main__':
     lambda_handler.run(debug=True)
